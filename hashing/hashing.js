6f7af1793b0403127431fa330f0d26abc4db394d
tomracia <tomasz.racia@gmail.com>
2021-09-12 19:20:37 +0200

    Part 1 and Part 2 of hashing exercise

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -1,43 +1,84 @@
-"use strict";
+'use strict';
 
-var crypto = require("crypto");
+const crypto = require('crypto');
 
-// The Power of a Smile
-// by Tupac Shakur
-var poem = [
-	"The power of a gun can kill",
-	"and the power of fire can burn",
-	"the power of wind can chill",
-	"and the power of a mind can learn",
-	"the power of anger can rage",
-	"inside until it tears u apart",
-	"but the power of a smile",
-	"especially yours can heal a frozen heart",
+// The Power of a Smile by Tupac Shakur
+const poem = [
+	'The power of a gun can kill',
+	'and the power of fire can burn',
+	'the power of wind can chill',
+	'and the power of a mind can learn',
+	'the power of anger can rage',
+	'inside until it tears u apart',
+	'but the power of a smile',
+	'especially yours can heal a frozen heart',
 ];
 
-var Blockchain = {
+const Blockchain = {
 	blocks: [],
 };
 
 // Genesis block
 Blockchain.blocks.push({
 	index: 0,
-	hash: "000000",
-	data: "",
+	hash: '000000',
+	data: '',
 	timestamp: Date.now(),
 });
 
-// TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
-
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
-
-
-// **********************************
-
-function blockHash(bl) {
-	return crypto.createHash("sha256").update(
-		// TODO: use block data to calculate hash
-	).digest("hex");
+function blockHash(block) {
+	return crypto
+		.createHash('sha256')
+		.update(`${block.index};${block.prevHash};${block.data};${block.timestamp};`)
+		.digest('hex');
 }
+
+function createBlock(data) {
+	let block = {
+		index: Blockchain.blocks.length,
+		prevHash: Blockchain.blocks[Blockchain.blocks.length - 1].hash,
+		data,
+		timestamp: Date.now(),
+	};
+
+	block.hash = blockHash(block);
+
+	Blockchain.blocks.push(block);
+
+	return block;
+}
+
+// Insert each line of poem into blockchain as a separate block
+for (let line of poem) {
+	createBlock(line);
+}
+
+function verifyBlock(block) {
+	const { index, hash, data, prevHash, timestamp } = block;
+	if (Number.isInteger(index) && index === 0 && hash === '000000') { return true; }
+
+	const blockHashData = { index, prevHash, data, timestamp };
+	if (
+		data && data !== ''
+		&& prevHash && prevHash !== ''
+		&& Number.isInteger(index) && index >= 0
+		&& blockHash(blockHashData) === hash) {
+		return true;
+	}
+
+	return false;
+}
+
+function verifyChain(blockchain) {
+	console.log(blockchain.blocks);
+
+	let isValid = true;
+	blockchain.blocks.forEach((block) => {
+		if (!verifyBlock(block)) { isValid = false; }
+	})
+
+	return isValid;
+}
+
+console.log(`\n\nBlockchain is valid: ${verifyChain(Blockchain)}`);
+
